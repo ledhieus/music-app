86a7b7f42b8946b7d5048274bc1555ac1e841509
ledhieus <ledan0602@gmail.com>
2024-08-17 08:45:02 +0700

    cài pug tạo thu muc view

--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ import express, {Express, Request, Response} from "express"
 const app: Express = express()
 const port: number = 3000
 
+//PUG
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+
 app.get("/topics", (req: Request, res: Response) => {
-    res.send("Chủ đề bài hát")
+    res.render("client/pages/topics/index")
 })
 
 app.listen(port, ()=>{
