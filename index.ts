15fb6288a7bf4d4123efab765839ad0413d7f2dd
ledhieus <ledan0602@gmail.com>
2024-08-17 09:48:11 +0700

    tạo route

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import express, {Express, Request, Response} from "express"
+import express, { Express } from "express"
 import dotenv from "dotenv"
 import * as database from "./config/database"
 
-import Topic from "./models/topic.model"
+import clientRoutes from "./routes/client/index.route"
+
 dotenv.config()
 
 database.connect()
@@ -14,13 +15,8 @@ const port: number | string = process.env.PORT || 3000
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
-app.get("/topics",async (req: Request, res: Response) => {
-    const topics = await Topic.find({
-        deleted: false
-    })
-    console.log(topics)
-    res.render("client/pages/topics/index")
-})
+// Client Router
+clientRoutes(app)
 
 app.listen(port, ()=>{
     console.log(`App listening on port ${port}`)
