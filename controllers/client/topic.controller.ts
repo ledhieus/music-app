74ea2b53474c9a67d21b03e71ee70be7a8dae524
ledhieus <ledan0602@gmail.com>
2024-08-17 10:57:30 +0700

    Làm trang chủ bài hat

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -6,9 +6,9 @@ export const topics = async (req: Request, res: Response) =>{
     const topics = await Topic.find({
         deleted: false
     })
-    console.log(topics)
 
     res.render("client/pages/topics/index", {
-        pageTitle: "Chủ đề bài hát"
+        pageTitle: "Chủ đề bài hát",
+        topics: topics
     })
 }
