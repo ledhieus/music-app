3621da8c99b76d73fcc580964a4560c8847504c7
ledhieus <ledan0602@gmail.com>
2024-08-21 16:58:45 +0700

    tìm kiếm hiển thị suggest

--- public/js/script.js
+++ public/js/script.js
@@ -87,3 +87,42 @@ if(listButtonFavorite.length > 0){
     })
 }
 //end Button favorite
+
+// Search Suggest
+const boxSearch = document.querySelector(".box-search")
+if(boxSearch) {
+    const input = boxSearch.querySelector("input[name='keyword']")
+    const boxSuggest = boxSearch.querySelector(".inner-suggest")
+
+    input.addEventListener("keyup", ()=> {
+        const keyword = input.value
+
+        const link = `/search/suggest?keyword=${keyword}`
+        
+        fetch(link)
+            .then(res => res.json())
+            .then(data => {
+                const songs = data.songs
+                if(songs.length > 0){
+                    boxSuggest.classList.add("show")
+                    const htmls = songs.map(song => {
+                        return `
+                            <a class="inner-item" href="/songs/detail/${song.slug}">
+                                <div class="inner-image"><img src="${song.avatar}"/></div>
+                                <div class="inner-info">
+                                    <div class="inner-title">${song.title}</div>
+                                    <div class="inner-singer><i class="fa-solid fa-microphone"></i> ${song.infoSinger.fullName}</div>
+                                </div>
+                            </a>
+                        `
+                    })
+                    const boxlist = boxSuggest.querySelector(".inner-list")
+                    boxlist.innerHTML = htmls.join("")
+                } else {
+                    boxSuggest.classList.remove("show")
+                }
+                
+            })
+    })
+}
+// End Search Suggest
