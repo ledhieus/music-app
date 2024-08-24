e66e011f084e05d4a19b61646624d3b6c3540204
ledhieus <ledan0602@gmail.com>
2024-08-24 09:09:47 +0700

    layout chung bên admin

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from "express"
 import dotenv from "dotenv"
+import path from "path"
 import * as database from "./config/database"
 
 import clientRoutes from "./routes/client/index.route"
+import adminRoutes from "./routes/admin/index.route"
+import { systemConfig } from "./config/config"
 
 dotenv.config()
 
@@ -16,6 +19,18 @@ app.use(express.static("public"))
 //PUG
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
+
+// TinyMCE
+app.use(
+    "/tinymce",
+    express.static(path.join(__dirname, "node_modules", "tinymce"))
+)
+
+//App local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+// admin Router
+adminRoutes(app)
 
 // Client Router
 clientRoutes(app)
